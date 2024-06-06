6581fcf946024d7516d77c9c326553c97f2891f1
naineelsoyantar <naineelsoyantar@gmail.com>
2024-06-06 17:37:45 +0000

    day2

---
+++ day2-shell-scripting/intro.sh
@@ -0,0 +1,24 @@
+echo 'naineel' | cat
+ls
+# ll
+ls -a
+# lsof
+pwd
+
+#creating a new file
+touch newfile
+
+#vim = create a file and write into file
+# vi newfile2
+
+#nproc  - cpus used by the ubuntu
+nproc
+
+free -m
+free -g
+
+df -h #human readable format of the all the disk storage space of the files on the os
+
+chmod a+rwx intro.sh
+
+chmod --help
